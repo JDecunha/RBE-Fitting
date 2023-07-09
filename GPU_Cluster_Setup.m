f11a649156cd74de931e05d35c371e0cc2456dd7
Joseph M DeCunha <joeydecunha@gmail.com>
2023-07-09 16:22:14 -0500

    gpu_config file update

--- GPU_Cluster_Setup.m
+++ GPU_Cluster_Setup.m
@@ -2,6 +2,10 @@ configCluster
 c = parcluster;
 c.AdditionalProperties.WallTime = '02:00';
 c.AdditionalProperties.MemUsage = '16';
-c.AdditionalProperties.AdditionalSubmitArgs = '-q egpu-medium -n 2 -gpu num=1:gmem=16';
+c.AdditionalProperties.GpusPerNode = 1;
+c.AdditionalProperties.GpuMemUsage = "16";
+c.AdditionalProperties.AdditionalSubmitArgs = '-n 2'; 
 c.saveProfile
+
+%-q egpu-medium -gpu num=1:gmem=16
 %j = c.batch(@H460_Linear_BWF_Fitting, 1,{},'AutoAddClientPath',true,'CurrentFolder','.');
