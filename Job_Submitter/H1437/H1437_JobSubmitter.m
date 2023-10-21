0e648e0eb05aef72dda349275d13f5e8ba261fd0
Joseph M DeCunha <joeydecunha@gmail.com>
2023-10-20 21:48:17 -0500

    Updating run scripts

--- Job_Submitter/H1437/H1437_JobSubmitter.m
+++ Job_Submitter/H1437/H1437_JobSubmitter.m
@@ -1,5 +1,5 @@
 %% Configure the script
-filePaths = ["H1437_dy/a.csv", "H1437_dy/b.csv" , "H1437_dy/c.csv", "H1437_dy/d.csv", "H1437_dy/e.csv", "H1437_dy/f.csv", "H1437_dy/g.csv", "H1437_dy/h.csv", "H1437_dy/i.csv", "H1437_dy/j.csv" , "H1437_dy/k.csv" , "H1437_dy/l.csv"];
+filePaths = ["H1437_fy/a.csv", "H1437_fy/b.csv" , "H1437_fy/c.csv", "H1437_fy/d.csv", "H1437_fy/e.csv", "H1437_fy/f.csv", "H1437_fy/g.csv", "H1437_fy/h.csv", "H1437_fy/i.csv", "H1437_fy/j.csv" , "H1437_fy/k.csv" , "H1437_fy/l.csv"];
 penaltyWeight = 0.; %typically make my penalty 30 when it's activated
 iterationsPerCyc = 250000;
 numCycles = 250;
@@ -16,7 +16,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1];
 
-batch(c, @Generic_BWF_RunScript, 1, {'linear_H1437_Pristine', "LinearBWF", "LinearPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'linear_H1437_fy', "LinearBWF", "LinearPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 
 %% Quadratic Fitting
@@ -26,7 +26,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1,0.1];
 
-batch(c, @Generic_BWF_RunScript, 1, {'quadratic_H1437_Pristine', "QuadraticBWF", "QuadraticBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'quadratic_H1437_fy', "QuadraticBWF", "QuadraticBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 
 %% Cubic Fitting
@@ -36,7 +36,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1,0.1,0.1];
 
-batch(c, @Generic_BWF_RunScript, 1, {'cubic_H1437_Pristine', "CubicBWF", "CubicBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'cubic_H1437_fy', "CubicBWF", "CubicBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 
 %% Fourth Fitting
@@ -46,7 +46,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1,0.1,0.1,0.1];
 
-batch(c, @Generic_BWF_RunScript, 1, {'fourth_H1437_Pristine', "FourthBWF", "FourthBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'fourth_H1437_fy', "FourthBWF", "FourthBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 
 %% Fifth Fitting
@@ -56,7 +56,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1,0.1,0.1,0.1,0.1];
 
-batch(c, @Generic_BWF_RunScript, 1, {'fifth_H1437_Pristine', "FifthBWF", "FifthBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'fifth_H1437_fy', "FifthBWF", "FifthBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 %% Q
 dynamicTemp = false;
@@ -65,7 +65,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1]; %Q, 2 params + beta
 
-batch(c, @Generic_BWF_RunScript, 1, {'Q_H1437_Pristine', "QBWF", "QPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'Q_H1437_fy', "QBWF", "QPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 %% QE
 dynamicTemp = false;
@@ -74,7 +74,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1,0.1]; %QE, 3 params + beta
 
-batch(c, @Generic_BWF_RunScript, 1, {'QE_H1437_Pristine', "QEBWF", "QEPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'QE_H1437_fy', "QEBWF", "QEPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 %% QE2
 dynamicTemp = false;
@@ -83,7 +83,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1,0.1]; %QE2, 3 params + beta
 
-batch(c, @Generic_BWF_RunScript, 1, {'QE2_H1437_Pristine', "QE2BWF", "QE2PenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'QE2_H1437_fy', "QE2BWF", "QE2PenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 %% LE
 dynamicTemp = false;
@@ -92,7 +92,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1,0.1]; %LE, 3 params + beta
 
-batch(c, @Generic_BWF_RunScript, 1, {'LE_H1437_Pristine', "LEBWF", "LEPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'LE_H1437_fy', "LEBWF", "LEPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 %% LQE
 dynamicTemp = false;
@@ -101,7 +101,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1,0.1,0.1]; %LQE, 4 params + beta
 
-batch(c, @Generic_BWF_RunScript, 1, {'LQE_H1437_Pristine', "LQEBWF", "LQEPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'LQE_H1437_fy', "LQEBWF", "LQEPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 %% LE2
 dynamicTemp = false;
@@ -110,7 +110,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1,0.1,0.1,0.1,0.1];
 
-batch(c, @Generic_BWF_RunScript, 1, {'LE2_H1437_Pristine', "LE2BWF", "LE2PenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'LE2_H1437_fy', "LE2BWF", "LE2PenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 %% LQE2
 dynamicTemp = false;
@@ -119,7 +119,7 @@ temps = [];
 
 InitialGuess = [0.1,0.1,0.1,0.1,0.1]; %LQE2, 4 params + beta
 
-batch(c, @Generic_BWF_RunScript, 1, {'LQE2_H1437_Pristine', "LQE2BWF", "LQE2PenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+batch(c, @Generic_BWF_RunScript, 1, {'LQE2_H1437_fy', "LQE2BWF", "LQE2PenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
 
 %% Gaussian Fitting
 dynamicTemp = false;
@@ -129,7 +129,7 @@ gradientAssist = true;
 InitialGuess = [0.1, 0.1, 0.1, 0.1, 0.1]; %Morstin inspired guess
 temps = [1, 5, 10, 10, 0.05];
 
-batch(c, @Generic_BWF_RunScript, 1, {'gaussian_H1437_Pristine', "GaussianBWF", "GaussianPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps})
+batch(c, @Generic_BWF_RunScript, 1, {'gaussian_H1437_fy', "GaussianBWF", "GaussianPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps})
 
 %% Skew Gaussian Fitting
 dynamicTemp = false;
@@ -138,7 +138,7 @@ gradientAssist = true;
 InitialGuess = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1];
 temps = [1, 1, 5, 5, 1, 0.05];
 
-batch(c, @Generic_BWF_RunScript, 1, {'skewGaussian_H1437_Pristine', "SkewGaussianBWF", "SkewGaussianPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps})
+batch(c, @Generic_BWF_RunScript, 1, {'skewGaussian_H1437_fy', "SkewGaussianBWF", "SkewGaussianPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps})
 
 %% Morstin fitting
 %[11460.000000, 2.5*power(10,-6), 2.1*power(10,-5), 2.*power(10,-7), 0.1]; Default guess
@@ -148,4 +148,4 @@ gradientAssist = true;
 InitialGuess = [11460.000000, 2.5*power(10,-6), 2.1*power(10,-5), 2.*power(10,-7), 0.1];
 temps = [1000, 1e-3, 1e-3, 1e-5, 0.1];
 
-batch(c, @Generic_BWF_RunScript, 1, {'morstin_H1437_Pristine', "MorstinBWF", "MorstinPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps})
+batch(c, @Generic_BWF_RunScript, 1, {'morstin_H1437_fy', "MorstinBWF", "MorstinPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps})
---
+++ Job_Submitter/H1437/H1437_fy_JobSubmitter.m
@@ -0,0 +1,151 @@
+%% Configure the script
+filePaths = ["H1437_dy/a.csv", "H1437_dy/b.csv" , "H1437_dy/c.csv", "H1437_dy/d.csv", "H1437_dy/e.csv", "H1437_dy/f.csv", "H1437_dy/g.csv", "H1437_dy/h.csv", "H1437_dy/i.csv", "H1437_dy/j.csv" , "H1437_dy/k.csv" , "H1437_dy/l.csv"];
+penaltyWeight = 0.; %typically make my penalty 30 when it's activated
+iterationsPerCyc = 250000;
+numCycles = 250;
+toleranceCycles = 10;
+
+%%
+c = parcluster('Desktop-10700k');
+%c = parcluster('GA401');
+
+%% Linear Fitting
+dynamicTemp = false;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1];
+
+batch(c, @Generic_BWF_RunScript, 1, {'linear_H1437_Pristine', "LinearBWF", "LinearPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+
+%% Quadratic Fitting
+dynamicTemp = false;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1,0.1];
+
+batch(c, @Generic_BWF_RunScript, 1, {'quadratic_H1437_Pristine', "QuadraticBWF", "QuadraticBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+
+%% Cubic Fitting
+dynamicTemp = true;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1,0.1,0.1];
+
+batch(c, @Generic_BWF_RunScript, 1, {'cubic_H1437_Pristine', "CubicBWF", "CubicBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+
+%% Fourth Fitting
+dynamicTemp = true;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1,0.1,0.1,0.1];
+
+batch(c, @Generic_BWF_RunScript, 1, {'fourth_H1437_Pristine', "FourthBWF", "FourthBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+
+%% Fifth Fitting
+dynamicTemp = true;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1,0.1,0.1,0.1,0.1];
+
+batch(c, @Generic_BWF_RunScript, 1, {'fifth_H1437_Pristine', "FifthBWF", "FifthBWFPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+%% Q
+dynamicTemp = false;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1]; %Q, 2 params + beta
+
+batch(c, @Generic_BWF_RunScript, 1, {'Q_H1437_Pristine', "QBWF", "QPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+%% QE
+dynamicTemp = false;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1,0.1]; %QE, 3 params + beta
+
+batch(c, @Generic_BWF_RunScript, 1, {'QE_H1437_Pristine', "QEBWF", "QEPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+%% QE2
+dynamicTemp = false;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1,0.1]; %QE2, 3 params + beta
+
+batch(c, @Generic_BWF_RunScript, 1, {'QE2_H1437_Pristine', "QE2BWF", "QE2PenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+%% LE
+dynamicTemp = false;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1,0.1]; %LE, 3 params + beta
+
+batch(c, @Generic_BWF_RunScript, 1, {'LE_H1437_Pristine', "LEBWF", "LEPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+%% LQE
+dynamicTemp = false;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1,0.1,0.1]; %LQE, 4 params + beta
+
+batch(c, @Generic_BWF_RunScript, 1, {'LQE_H1437_Pristine', "LQEBWF", "LQEPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+%% LE2
+dynamicTemp = false;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1,0.1,0.1,0.1,0.1];
+
+batch(c, @Generic_BWF_RunScript, 1, {'LE2_H1437_Pristine', "LE2BWF", "LE2PenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+%% LQE2
+dynamicTemp = false;
+gradientAssist = true;
+temps = [];
+
+InitialGuess = [0.1,0.1,0.1,0.1,0.1]; %LQE2, 4 params + beta
+
+batch(c, @Generic_BWF_RunScript, 1, {'LQE2_H1437_Pristine', "LQE2BWF", "LQE2PenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps});
+
+%% Gaussian Fitting
+dynamicTemp = false;
+gradientAssist = true;
+
+
+InitialGuess = [0.1, 0.1, 0.1, 0.1, 0.1]; %Morstin inspired guess
+temps = [1, 5, 10, 10, 0.05];
+
+batch(c, @Generic_BWF_RunScript, 1, {'gaussian_H1437_Pristine', "GaussianBWF", "GaussianPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps})
+
+%% Skew Gaussian Fitting
+dynamicTemp = false;
+gradientAssist = true;
+
+InitialGuess = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1];
+temps = [1, 1, 5, 5, 1, 0.05];
+
+batch(c, @Generic_BWF_RunScript, 1, {'skewGaussian_H1437_Pristine', "SkewGaussianBWF", "SkewGaussianPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps})
+
+%% Morstin fitting
+%[11460.000000, 2.5*power(10,-6), 2.1*power(10,-5), 2.*power(10,-7), 0.1]; Default guess
+dynamicTemp = false;
+gradientAssist = true;
+
+InitialGuess = [11460.000000, 2.5*power(10,-6), 2.1*power(10,-5), 2.*power(10,-7), 0.1];
+temps = [1000, 1e-3, 1e-3, 1e-5, 0.1];
+
+batch(c, @Generic_BWF_RunScript, 1, {'morstin_H1437_Pristine', "MorstinBWF", "MorstinPenaltyFunction", filePaths, InitialGuess, penaltyWeight, iterationsPerCyc, numCycles, toleranceCycles, dynamicTemp, gradientAssist, temps})
