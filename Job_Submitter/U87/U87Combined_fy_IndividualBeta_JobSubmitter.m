1ef8a056d16f09273e705be88cb25dd4e131ac3d
Joseph M DeCunha <joeydecunha@gmail.com>
2023-11-24 15:16:28 -0600

    add semicolon

--- Job_Submitter/U87/U87Combined_fy_IndividualBeta_JobSubmitter.m
+++ Job_Submitter/U87/U87Combined_fy_IndividualBeta_JobSubmitter.m
@@ -67,7 +67,7 @@ c.AdditionalProperties.MemUsage = 16.;
 c.AdditionalProperties.GpusPerNode = 1;
 c.AdditionalProperties.GpuMemUsage = 16.;
 c.AdditionalProperties.QueueName = 'egpu-medium';
-c.AdditionalProperties.AdditionalSubmitArgs = '-n 10' % -q egpu-medium -gpu num=1:gmem=16'; 
+c.AdditionalProperties.AdditionalSubmitArgs = '-n 10'; % -q egpu-medium -gpu num=1:gmem=16'; 
 c.saveProfile
 
 %% Linear Fitting
