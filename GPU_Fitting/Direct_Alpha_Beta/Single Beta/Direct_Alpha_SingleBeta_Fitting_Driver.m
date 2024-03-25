ab5cb172a47daa09c81fa9b1aabd7c6e4165861a
Joseph M DeCunha <joeydecunha@gmail.com>
2024-03-25 15:53:32 -0500

    Implement direct alpha beta fitting with only a single beta

---
+++ GPU_Fitting/Direct_Alpha_Beta/Single Beta/Direct_Alpha_SingleBeta_Fitting_Driver.m
@@ -0,0 +1,19 @@
+function [output] = Direct_Alpha_SingleBeta_Fitting_Driver(experimentalData, initialGuess)
+
+%Construct the cost function
+CostFunc = @(x) DirectAlpha_SingleBeta_CostFunction(x, experimentalData);
+
+%Set up the fitting options for gradient descent
+optionsGradientDescent = optimoptions('fmincon','MaxFunctionEvaluations',1e6);
+lowerBounds = zeros(size(initialGuess,1),1);
+%optionsGradientDescent.MaxFunctionEvaluations = 1e6;
+
+%Using hybrid approach, run gradient descent
+[GradientSoln, GradientCost] = fmincon(CostFunc,initialGuess,[],[],[],[],lowerBounds,[],[],optionsGradientDescent);
+
+
+%Smush into output
+output = {GradientSoln, GradientCost};
+
+end
+
---
+++ GPU_Fitting/Direct_Alpha_Beta/Single Beta/RunScript_Direct_Alpha_SingleBeta.m
@@ -0,0 +1,26 @@
+function [output] = RunScript_Direct_Alpha_SingleBeta(name, filePaths)
+
+%% Setup environment
+addpath(genpath("."));
+%% Import the survival data
+%A 3d array to hold the experiments data
+experiments = [];
+
+%Loop through the files and concatenate
+for i = 1:size(filePaths,2)
+    experiments = cat(3,experiments, readmatrix(filePaths(i)));
+end
+
+GPUExperimentalData = ExperimentData(experiments);
+
+%% Call the driver
+InitialGuess = zeros(size(GPUExperimentalData.SF,3)*2,1)+0.1;
+
+output = Direct_Alpha_SingleBeta_Fitting_Driver(GPUExperimentalData, InitialGuess);
+
+input = [name, filePaths];
+
+output = {output, input};
+
+end
+
